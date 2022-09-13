5fba6abb79703f352880bfb901493ca7086c39ec
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-09-13 11:08:36 -0600

    Add db setup script for multi-worker

---
+++ depolarizing_chatroom/setup.py
@@ -0,0 +1,15 @@
+from .data.crud import build_prod_database
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help="Force rebuild of production database",
+    )
+    args = parser.parse_args()
+
+    build_prod_database(args.force)
