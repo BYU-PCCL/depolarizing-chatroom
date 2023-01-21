e5dc74bda74b67b45261457b5fc129609e3f5ca0
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2023-01-21 12:54:47 -0700

    Remove manual log formatting setup

--- depolarizing_chatroom/logger.py
+++ depolarizing_chatroom/logger.py
@@ -12,19 +12,6 @@ logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
-# Create formatter
-# formatter = logging.Formatter(
-#     "%(asctime)s [%(process)d] [%(levelname)s] [%(name)s] %(message)s",
-#     "[%Y-%m-%d %H:%M:%S %z]",
-# )
-#
-# # Add formatter to ch
-# ch.setFormatter(formatter)
-#
-# Add ch to logger
-# logger.addHandler(ch)
-
-
 def format_parameterized_log_message(message: str, **kwargs) -> str:
     params_items = [f"{k}={repr(v)}" for k, v in kwargs.items()]
     params_str = f' [{", ".join(params_items)}]' if kwargs else ""
